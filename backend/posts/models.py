9abace5557314897d5bb95fdd644d1f732544afb
Brian Liu <16676830+branlulu@users.noreply.github.com>
2022-02-27 22:03:14 -0500

    Add fields for Post

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,36 @@
+from tkinter import CASCADE
 from django.db import models
+import os
+
+# Title
+# Content
+# Date Posted
+# Author
+# Price
+# BuyOrSell
+# Images
+# isSold
+# Category
+# isAnonymous
+# isDraft
+
+def get_image_path(instance, filename):
+    return os.path.join('data', 'posts', str(instance.id), filename)
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=32)
+    content = models.CharField(max_length=63206) # FB's max char count
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE) # TODO: check with User and integrate
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    buy = models.BooleanField(null=False) # True for buy, False for sell
+    images = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    isSold = models.BooleanField(default=False)
+    category = models.CharField(max_length=32)
+    isAnonymous = models.BooleanField(default=False)
+    isDraft = models.BooleanField(default=False)
+
+
+
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model): # placeholder for now
+    id = models.AutoField(primary_key=True)
+    pass
