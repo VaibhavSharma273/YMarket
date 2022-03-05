fdb73e57826c8a68abd5789267ed9ab641c825d2
Austin Chen <austinchen@Austins-Air.wireless.yale.internal>
2022-03-05 16:02:48 -0500

    added JsonConfirmEmailView

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, re_path
-from dj_rest_auth.registration.views import RegisterView, VerifyEmailView
+from dj_rest_auth.registration.views import RegisterView, VerifyEmailView, ResendEmailVerificationView
 from dj_rest_auth.views import LoginView, LogoutView
-from allauth.account.views import ConfirmEmailView
+from users.views import JsonConfirmEmailView
 
 urlpatterns = [
-    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('account-confirm-email/<str:key>/', JsonConfirmEmailView.as_view()),
+    path('register/', RegisterView.as_view(), name='account_signup'),
+    path('login/', LoginView.as_view(), name='account_login'),
+    path('logout/', LogoutView.as_view(), name='account_logout'),
 
+    path('resend-verification/', ResendEmailVerificationView.as_view(), name='account_resend_email_verification'),
     path('verify-email/', VerifyEmailView.as_view()),
     path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
