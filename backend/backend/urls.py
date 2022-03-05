dedeca1ac754e3c4d9f40e64ad1a0251284d4c09
Austin Chen <austinchen@Austins-Air.wireless.yale.internal>
2022-03-05 16:13:55 -0500

    modified password reset paths

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    
-    path('password-reset', PasswordResetView.as_view()),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from dj_rest_auth.registration.views import RegisterView, VerifyEmailView, ResendEmailVerificationView
 from dj_rest_auth.views import LoginView, LogoutView
 from users.views import JsonConfirmEmailView
+from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 
 urlpatterns = [
     path('account-confirm-email/<str:key>/', JsonConfirmEmailView.as_view()),
@@ -14,4 +15,7 @@ urlpatterns = [
     path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
          VerifyEmailView.as_view(), name='account_confirm_email'),
+
+    path('password-reset', PasswordResetView.as_view()),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
