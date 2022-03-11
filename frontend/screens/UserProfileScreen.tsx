9715da7addb46ded2997f3d190b944a2356d6096
Ellen <Ellen@MacBook-Pro-3.local>
2022-03-11 14:59:24 -0500

    updated profile view to get rid of segemented tabs

--- frontend/screens/UserProfileScreen.tsx
+++ frontend/screens/UserProfileScreen.tsx
@@ -7,60 +7,18 @@ import { RootTabScreenProps } from '../types';
 import ProfilePhoto from '../components/ProfilePhoto';
 import { ScrollView } from 'react-native-gesture-handler';
 
-// import { Card, Icon } from 'react-native-elements'
-import SegmentedControlTab from "react-native-segmented-control-tab";
-// import SegmentedControl from 'rn-segmented-control';
-
 export default function UserProfileScreen({ navigation } : RootTabScreenProps<'UserProfile'>) {
     const [user, setUser] = useState(null);
-    // const [tabIndex, setTabIndex] = React.useState(0);
-
-    const [selectedIndex, setSelectedIndex] = useState(0);
-
-    const handleSingleIndexSelect = (index: any) => {
-        // For single Tab Selection SegmentedControlTab
-        setSelectedIndex(index);
-    };
-
+    
     return (
-        // <SafeAreaView style={styles.container}>
-            
-        // </SafeAreaView>
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={{marginVertical: 20, height: 1, width: '80%'}}/> 
-                <ProfilePhoto/>
-                <Text style={styles.title}>User Profile Name</Text>
-                <View style={{marginVertical: 5, height: 1, width: '80%'}}/> 
-                <Text style={styles.contact}>User Profile Email</Text> 
-                <View style={styles.separator}/>
-                <SegmentedControlTab
-                    values={['Bought', 'Sold']}
-                    selectedIndex={selectedIndex}
-                    onTabPress={handleSingleIndexSelect}
-                    borderRadius={0}
-                    tabsContainerStyle={{ height: 50, backgroundColor: '#FFFFFF' }}
-                    tabStyle={{
-                        backgroundColor: '#F2F2F2',
-                        borderWidth: 0,
-                        borderColor: 'transparent',
-                    }}
-                    activeTabStyle={{ backgroundColor: 'white', marginTop: 2 }}
-                    tabTextStyle={{ color: '#bd5319', fontWeight: 'bold' }}
-                    activeTabTextStyle={{ color: '#5f712d' }}
-                />
-                {selectedIndex === 0 && (
-                    <Text style={styles.tabContent}>Purchased Items
-
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </Text>
-                )}
-                {selectedIndex === 1 && (
-                    <Text style={styles.tabContent}> Sold Items</Text>
-                )}
-            </View>
-        </ScrollView>
+        <View style={styles.container}>
+            <View style={{marginVertical: 20, height: 1, width: '80%'}}/> 
+            <ProfilePhoto/>
+            <Text style={styles.title}>User Profile Name</Text>
+            <View style={{marginVertical: 5, height: 1, width: '80%'}}/> 
+            <Text style={styles.contact}>User Profile Email</Text> 
+            <View style={styles.separator}/>
+        </View>
 
     )
 }
@@ -70,7 +28,6 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
-      paddingTop: StatusBar.currentHeight,
     },
     title: {
       fontSize: 20,
@@ -85,10 +42,5 @@ const styles = StyleSheet.create({
       marginVertical: 8,
       height: 1,
       width: '80%',
-    },
-    tabContent: {
-        color: '#444444',
-        fontSize: 18,
-        margin: 24,
     },
   });
